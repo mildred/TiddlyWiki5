2877e7897582bb9fe7ce64f3cb2f8646fa6008dd
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2013-11-14 15:35:10 +0100

    fixup javascript dta provider (catch exceptions)

--- core/modules/dataproviders/javascript.js
+++ core/modules/dataproviders/javascript.js
@@ -15,8 +15,12 @@ The JSON data provider provides data for an application/javascript tiddler
 var JavaScriptDataProvider = function(tiddler,defaultData,options) {
 	// Parse javascript, expect a function
   // Call the function with this bound to the tiddler
-  var fn = $tw.utils.evalGlobal("exports = (function(){" + tiddler.fields.text + "});", {}, tiddler.fields.title);
-  var data = fn.call(tiddler);
+  try {
+    var fn = $tw.utils.evalGlobal("exports = (function(){" + tiddler.fields.text + "});", {}, tiddler.fields.title);
+    var data = fn.call(tiddler);
+  } catch(e) {
+    console.log("Error in JavaScript DataProvider in " + tiddler.fields.title + ": " + e.toString(), e);
+  }
   return data;
 };
 
